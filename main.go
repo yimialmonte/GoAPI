25e02a5ae14f462379b3cd1e2f543fef0dc7a3d5
yimialmonte <yelmialmonte@gmail.com>
2021-04-20 09:20:51 -0400

    created root handler route

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import (
 	"net/http"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Running API v1\n"))
+}
+
 func main() {
+	http.HandleFunc("/", rootHandler)
 	err := http.ListenAndServe("localhost:11111", nil)
 	if err != nil {
 		log.Fatal(err)
